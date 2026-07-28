4028a3e946629a4964f96bf3866b5001ca9efc11
Max Frohman <mbf1102@rit.edu>
2026-07-28 14:11:24 -0400

    minor work on prepping custom logger, change to autoallocating p

--- src/python/logger.py
+++ src/python/logger.py
@@ -1,25 +1,25 @@
 import logging
+import re
+
+from colorama import Fore, Back, Style
+
 
 SUCCESS_LEVEL = 21
 
-logging.addLevelName(SUCCESS_LEVEL, "SUCCESS")
-
-
-class ANSIColors:
-    GRAY = "\033[90m"
-    GREEN = "\033[92m"
-    BOLD = "\033[1m"
-    OFF = "\033[0m"
+logging.addLevelName(SUCCESS_LEVEL, f"{Fore.GREEN}SUCCESS{Fore.RESET}")
 
 
 class CustomLogger(logging.getLoggerClass()):
     def debug(self, message, *args, **kwargs):
-        message = ANSIColors.GRAY + message + ANSIColors.OFF
-        super().debug(message, *args, **kwargs)
+        message = Style.DIM + message + Style.RESET_ALL
+        super().debug(message, *args, **kwargs, stacklevel=2)
 
     def success(self, message, *args, **kwargs):
+        message = re.sub(r"success\w*", lambda m: f"{Fore.GREEN}{m.group(0)}{Fore.RESET}", message, flags=re.IGNORECASE)
         if self.isEnabledFor(SUCCESS_LEVEL):
             self._log(SUCCESS_LEVEL, message, args, **kwargs, stacklevel=2)
+        else:
+            self._log(logging.INFO, message, args, **kwargs, stacklevel=2)
 
 
 logging.setLoggerClass(CustomLogger)
